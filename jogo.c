4a7ccd26fae5052c3a362edc5093b72a7de97040
John-IoT <contato.joaodantas@gmail.com>
2025-05-15 13:36:31 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+void funcaoTeste(void){
+    printf("Funcao com finalidade para testes\n");
+    printf("Hello World!\n");
+}
 
 int main()
 {
     funcaoTeste();
+    return 0;
 }
 
-void funcaoTeste(void){
-    printf("Funcao com finalidade para testes\n");
-    printf("Hello World!\n");
-
-}
