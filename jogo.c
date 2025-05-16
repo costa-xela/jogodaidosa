c823d2c7c151a571f05a4d8a08fe31db9322b973
Alvaro3112 <137121425+Alvaro3112@users.noreply.github.com>
2025-05-15 22:28:52 -0300

    Update jogo.c

--- jogo.c
+++ jogo.c
@@ -1,10 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 //menu do jogo
 
 void limpaTela(){
     system("clear");
+}
+void jogo(char nomeDoJogadorUm [], char nomeDoJogadorDois [], int pontuacaoJogadorUm, int pontuacaoJogadorDois){
+ 
+    ///Variáveis Gerais
+    char nomeDoJogadorAtual [30];//Nomes dos jogadores
+    char tabuleiro[3][3];                                       //Tabuleiro do Jogo
+    int linha,coluna;                                           //Auxiliares para tabuleiro
+    int linhaJogada,colunaJogada,posicaoJogada;                 //Posição em que o jogador posiciona sua marca
+    int estadoDeJogo = 1;                                       //0 = Sem jogo,1 = Em Jogo
+    int turnoDoJogador = 1;                                     //1 = X, 2 = O
+    int rodada = 0;                                             //Quantas vezes os jogadores jogaram no total
+    int opcao;                                                  //Opção de reinício
+    bool posicionouJogada;                                      //Verifica se o jogador colocou um marcador no tabuleiro
+ 
+    //Coloca os '-' no tabuleiro para indicar o vazio
+    iniciaTabuleiro(tabuleiro);
+ 
+    while(rodada < 9 && estadoDeJogo == 1){
+ 
+        limpaTela();
+ 
+        printf("\nRodada: %i \n",rodada);
+        printf("Pontuacao: %s %i  x  %i %s",nomeDoJogadorUm,pontuacaoJogadorUm,pontuacaoJogadorDois,nomeDoJogadorDois);
+ 
+        //Exibe o tabuleiro na tela
+        exibeTabuleiro(tabuleiro);
+ 
+        //Exibe qual numero corresponde a qual posicao
+        exibeInstrucoes();
+ 
+ 
+        //Atualiza o nome do jogador atual
+        if(turnoDoJogador == 1){
+ 
+            strcpy(nomeDoJogadorAtual,nomeDoJogadorUm);
+
+            
+ 
+        }else{
+ 
+            strcpy(nomeDoJogadorAtual,nomeDoJogadorDois);
+ 
+        }
+ 
+        posicionouJogada = false;
+ 
+        //Matriz de posicoes possíveis
+        int posicoes[9][2] = {{2,0},{2,1},{2,2},{1,0},{1,1},{1,2},{0,0},{0,1},{0,2}};
+ 
+        while(posicionouJogada == false){
+ 
+            //Lê a jogada
+            printf("\nDigite uma posicao conforme o mapa acima:",nomeDoJogadorAtual);
+            scanf("%c",&posicaoJogada); 
+ 
+            //Passa a linha e coluna de acordo com a matriz de posições exibida no mapa
+            linhaJogada = posicoes[posicaoJogada-1][0];
+            colunaJogada = posicoes[posicaoJogada-1][1];
+ 
+            //Verifica se a posição é vazia
+            if(tabuleiro[linhaJogada][colunaJogada] == '-'){
+ 
+                //Conseguiu posicionar um marcador
+                posicionouJogada = true;
+ 
+                //Verifica de quem é a vez para posicionar o marcador
+                if(turnoDoJogador == 1){
+ 
+                    tabuleiro[linhaJogada][colunaJogada] = 'X';
+ 
+                    turnoDoJogador = 2;
+ 
+                }else{
+ 
+                    tabuleiro[linhaJogada][colunaJogada] = 'O';
+ 
+                    turnoDoJogador = 1;
+ 
+                }
+ 
+            }
+ 
+        }
+ 
+        //Confere se o jogo acabou
+        if(confereTabuleiro(tabuleiro) == 1){
+            printf("O jogador X venceu");
+            pontuacaoJogadorUm++;
+            estadoDeJogo = 0;
+        }else if(confereTabuleiro(tabuleiro) == 2){
+            printf("O jogador O venceu");
+            pontuacaoJogadorDois++;
+            estadoDeJogo = 0;
+        }
+ 
+        //Aumenta uma rodada
+        rodada++;
+ 
+    }
+ 
+    exibeTabuleiro(tabuleiro);
+    printf("Fim de jogo");
+    printf("\nO que deseja fazer?");
+    printf("\n1-Continuar Jogando");
+    printf("\n2-Menu Inicial");
+    printf("\n3-Sair");
+    scanf("%i",&opcao);
+    if(opcao == 1){
+        jogo(nomeDoJogadorUm, nomeDoJogadorDois,pontuacaoJogadorUm,pontuacaoJogadorDois);
+    }else if(opcao == 2){
+        menuInicial();
+    }
+ 
 }
 void menuInicial(){
  
